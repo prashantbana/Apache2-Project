a9abcb9d093787bd9744978b4f05497d5659efd8
prashantbana <prashantsinghbana673@gmail.com>
2017-07-02 18:03:21 +0530

    Changes in conf files and installation script

--- install.sh
+++ install.sh
@@ -7,13 +7,13 @@ sudo cp html/index.html /var/www/html/site2/
 
 
 sudo cp conf/site1.com.conf /etc/apache2/sites-available/
-sudo cp conf/site1ssl.com.conf /etc/apahce2/sites-available/
+sudo cp conf/site1ssl.com.conf /etc/apache2/sites-available/
 
 ip=sudo ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1 -d'/'
 
 
-#echo "$ip:443" >> /etc/apache2/ports.conf
-#echo "$ip:80" >> /etc/apache2/ports.conf
+echo "$ip:443" >> sudo tee --append /etc/apache2/ports.conf
+echo "$ip:80" >> sudo tee --append /etc/apache2/ports.conf
 
 sudo systemctl start apache2
 sudo a2enmod ssl
